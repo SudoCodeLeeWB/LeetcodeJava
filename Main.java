e736c9aa6caedf5ea85f3e8d959376d75d0e03a5
Lee Wonbeen <84519146+SudoCodeLeeWB@users.noreply.github.com>
2022-04-21 09:38:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+	  
+    // Entery point for  each Days => call as a method here, declare the shell file as a static. This file is used as entery point. 
+
+	  //Day 1. 
+	  
+	  }
+}
