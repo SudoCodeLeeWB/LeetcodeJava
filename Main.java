2fafad5150091abdd965279d622bdc341b84e6c6
LeeWonBeen /  이원빈 <84519146+SudoCodeLeeWB@users.noreply.github.com>
2022-04-22 19:22:26 +0000

    check init.

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 class Main {
   public static void main(String[] args) {
 	  
-    // Entery point for  each Days => call as a method here, declare the shell file as a static. This file is used as entery point. 
+    // Entery point for each Files => call as a method here, declare the shell file as a static. This file is used as entery point. 
 
 	  //Day 1. 
+	  System.out.println("testing entery point.");
+	  
 	  
 	  }
 }
